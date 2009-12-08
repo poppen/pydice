803ccc210a1dddf8a06c724bb47fc8785c94bb51
Shinsuke Matsui <poppen.jp@gmail.com>
2009-12-08 16:27:42 +0900

    add roll()

--- dice.py
+++ dice.py
@@ -7,6 +7,66 @@ class Dice:
 	def __init__(self):
 		self.throws = []
 
+	def roll(self, line):
+
+		prog = re.compile(r"""
+			^                 # beginning of line
+			(                 # dice string in group(1)
+				(?:\d+)?      # optional count
+				[dD]          # 'd' for dice
+				(?:           # type of dice:
+					\d+       # either one or more digits
+					|         # or
+					%         # a percent sign for d% = d100
+				)
+			)
+			(?:               # grouping-only parens
+				([-+xX*/bB])  # a + - * / b(est) in group(2)
+				(\d+)         # an offset in group(3)
+			)?                # both of those last are optional
+		""", re.VERBOSE)
+
+		match = prog.match(line)
+		if match == None:
+			return
+
+		dice_string = match.group(1)
+
+		sign = match.group(2)
+		if sign != None:
+			sign = sign.lower()
+
+		offset = match.group(3)
+		if offset != None:
+			offset = int(offset)
+
+		throws = self.roll_list(dice_string)
+		if len(throws) == 0:
+			return
+
+		if sign == 'b':
+			throws.sort()
+			throws.reverse()
+			throws = throws[0:offset]
+
+		result = 0
+		result += sum(throws)
+
+		if sign == '+':
+			result += offset
+
+		if sign == '-':
+			result -= offset
+
+		if sign == '*' or sign == 'x':
+			result *= offset
+
+		if sign == '/':
+			result /= offset
+			result = int(result)
+
+		return result
+
 	def roll_list(self, dice_string):
 
 		prog = re.compile(r"""
